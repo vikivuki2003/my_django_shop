08a1faff525c322df8673724749bb6cb9906c804
Viktoriia Diukina <viki_dyukina@mail.ru>
2024-11-25 17:41:19 +0400

    Delete payment directory

--- payment/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import PaymentMethod, Payment
-
-admin.site.register(PaymentMethod)
-admin.site.register(Payment)
--- payment/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class CheckoutForm(forms.Form):
-    full_name = forms.CharField(max_length=100, required=True, label="Full name")
-    email = forms.EmailField(required=True, label="Email address")
-    street_address = forms.CharField(max_length=255, required=True, label="Street address")
-    apartment_address = forms.CharField(max_length=255, required=False, label="Apartment address")
-    country = forms.CharField(max_length=100, required=False, label="Country")
-    zipcode = forms.CharField(max_length=20, required=False, label="Zip code (Optional)")
